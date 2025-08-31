5480a7aae59e0cfc1f5695160c02f4052fd28eba
SilverPhoenix99 <antoniopedrosilvapinto@gmail.com>
2025-08-31 20:30:43 +0100

    Improved Invoke-WithEnv to  Append and Prepend to environment variables

--- Invoke-WithEnv.ps1
+++ Invoke-WithEnv.ps1
@@ -1,31 +1,78 @@
-
+<#
+Cross-platform `env`
+#>
 function Invoke-WithEnv {
 
-    param (
-        [Parameter(Mandatory)]
-        [ValidateNotNull()]
-        [hashtable] $EnvVars,
+    [CmdletBinding(DefaultParameterSetName = 'Default')]
+    param(
+        [Parameter(Mandatory, ParameterSetName = 'ScriptSet', Position = 1)]
+        [Parameter(Mandatory, ParameterSetName = 'ScriptSetPrepend', Position = 0)]
+        [Parameter(Mandatory, ParameterSetName = 'ScriptSetAppend', Position = 0)]
+        [Parameter(Mandatory, ParameterSetName = 'ScriptSetPrependAppend', Position = 0)]
+        [Parameter(Mandatory, ParameterSetName = 'ScriptPrepend', Position = 0)]
+        [Parameter(Mandatory, ParameterSetName = 'ScriptAppend', Position = 0)]
+        [Parameter(Mandatory, ParameterSetName = 'ScriptPrependAppend', Position = 0)]
+        [scriptblock] $ScriptBlock,
 
-        [Parameter(Mandatory)]
-        [ValidateNotNull()]
-        [scriptblock] $Script
+        [Parameter(Mandatory, ParameterSetName = 'ScriptSet', Position = 0)]
+        [Parameter(Mandatory, ParameterSetName = 'ScriptSetPrepend')]
+        [Parameter(Mandatory, ParameterSetName = 'ScriptSetAppend')]
+        [Parameter(Mandatory, ParameterSetName = 'ScriptSetPrependAppend')]
+        [hashtable] $Set,
+
+        [Parameter(Mandatory, ParameterSetName = 'ScriptSetPrepend')]
+        [Parameter(Mandatory, ParameterSetName = 'ScriptSetPrependAppend')]
+        [Parameter(Mandatory, ParameterSetName = 'ScriptPrepend')]
+        [Parameter(Mandatory, ParameterSetName = 'ScriptPrependAppend')]
+        [hashtable] $Prepend,
+
+        [Parameter(Mandatory, ParameterSetName = 'ScriptSetAppend')]
+        [Parameter(Mandatory, ParameterSetName = 'ScriptSetPrependAppend')]
+        [Parameter(Mandatory, ParameterSetName = 'ScriptAppend')]
+        [Parameter(Mandatory, ParameterSetName = 'ScriptPrependAppend')]
+        [hashtable] $Append
     )
 
+    $callerErrorActionPreference = $ErrorActionPreference
+    $ErrorActionPreference = [Management.Automation.ActionPreference]::Stop
+
+    if ($PSCmdlet.ParameterSetName -eq 'Default') {
+        Get-Item Env:*
+        return
+    }
+
+    $keys = ($Set.Keys + $Prepend.Keys + $Append.Keys).Where({ $_ })
     $oldEnv = @{}
-    foreach ($name in $EnvVars.Keys) {
-        $oldEnv[$name] = (Get-Item "Env:$name" -ErrorAction SilentlyContinue).Value
+    foreach ($name in $keys) {
+        $oldEnv[$name] = (Get-Item -LiteralPath "Env:$name" -ErrorAction Ignore).Value
     }
 
     try {
-        foreach ($var in $EnvVars.GetEnumerator()) {
-            Set-Item "Env:$($var.Name)" $var.Value
+        foreach ($var in ${Set}?.GetEnumerator()) {
+            Set-Item -LiteralPath "Env:$($var.Name)" -Value $var.Value
         }
 
-        & $Script
+        foreach ($var in ${Prepend}?.GetEnumerator()) {
+            $value = (Get-Item -LiteralPath "Env:$($var.Name)" -ErrorAction Ignore).Value ?? ''
+            $value = @($var.Value, $value) -join [IO.Path]::PathSeparator
+            Set-Item -LiteralPath "Env:$($var.Name)" -Value $value
+        }
+
+        foreach ($var in ${Append}?.GetEnumerator()) {
+            $value = (Get-Item -LiteralPath "Env:$($var.Name)" -ErrorAction Ignore).Value ?? ''
+            $value = @($value, $var.Value) -join [IO.Path]::PathSeparator
+            Set-Item -LiteralPath "Env:$($var.Name)" -Value $value
+        }
+
+        & $ScriptBlock
+    }
+    catch {
+        $global:Error.RemoveAt(0)
+        Write-Error $_ -ErrorAction $callerErrorActionPreference
     }
     finally {
         foreach ($var in $oldEnv.GetEnumerator()) {
-            Set-Item "Env:$($var.Name)" $var.Value
+            Set-Item -LiteralPath "Env:$($var.Name)" -Value $var.Value
         }
     }
 }
